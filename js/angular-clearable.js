6f16efe980082ad21e6f83844c80de1e7a83a941
Miguel Mota <miguelmota2@gmail.com>
2014-05-25 15:20:17 -0700

    vertically align clear button

--- js/angular-clearable.js
+++ js/angular-clearable.js
@@ -40,8 +40,9 @@ angular.module('xngClearable', []).
                     var btn = tElement.next();
 
                     btn.css('font-size', Math.round(tElement.prop('offsetHeight')*0.8) + 'px');
-                    btn.css('top', '2px');
+                    btn.css('top', '50%');
                     btn.css('left', Math.round(tElement.prop('offsetWidth') - btn.prop('offsetWidth')*1.3) + 'px');
+                    btn.css('margin', [-(btn.prop('offsetHeight') / 2) + 'px',0,0,0].join(' '));
 
                     return function(scope, iElement, iAttrs) {
                         if (iElement[0].tagName == 'DIV') {
